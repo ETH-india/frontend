a2dbd06623f1a816cf6b19b685ccdea1ebc67821
Farhat Kadiwala <farhat.kadiwala03@gmail.com>
2023-12-10 05:12:20 +0530

    landing page completed

---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import SubSection from "./SubSection";
+const page = () => {
+  return (
+    <>
+      <SubSection />
+    </>
+  );
+};
+export default page;
